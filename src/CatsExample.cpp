2fbe594a3ed71cb735e2e00541238ba5e655bb7c
Andreas Remar <andreas.remar@gmail.com>
2017-10-22 18:54:47 +0200

    Create a sprite instance.

--- src/CatsExample.cpp
+++ src/CatsExample.cpp
@@ -4,7 +4,21 @@
 int main() {
   Cats::Init(640, 480);
   Cats::SetBackgroundColor(0xff, 0, 0);
-  Cats::Redraw(0);
   Cats::LoadSprite("../data/sprite.json");
+  Cats::CreateSpriteInstance("sprite");
+
+  int startTime = SDL_GetTicks();
+  int lastFrameTime = SDL_GetTicks();
+  bool running = true;
+
+  while(running) {
+    float delta = (SDL_GetTicks() - lastFrameTime) / 1000.0f;
+    lastFrameTime = SDL_GetTicks();
+    Cats::Redraw(delta);
+    if(SDL_GetTicks() - startTime > 3000) {
+      running = false;
+    }
+  }
+
   return 0;
 }
