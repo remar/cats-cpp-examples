f32e12125ee18eb2d05c1ffdfb54013b837a58f3
Andreas Remar <andreas.remar@gmail.com>
2017-10-28 21:17:44 +0200

    Quit with escape, show/hide sprite with H.

--- src/CatsExample.cpp
+++ src/CatsExample.cpp
@@ -9,17 +9,29 @@ int main() {
   Cats::RemoveSpriteInstance(spriteId);
   spriteId = Cats::CreateSpriteInstance("sprite");
 
-  int startTime = SDL_GetTicks();
   int lastFrameTime = SDL_GetTicks();
   bool running = true;
+  bool visible = true;
+  SDL_Event event;
 
   while(running) {
+    while(SDL_PollEvent(&event)) {
+      if(event.type == SDL_QUIT) {
+	running = false;
+      } else if(event.type == SDL_KEYDOWN && event.key.repeat == 0) {
+	if(event.key.keysym.sym == SDLK_ESCAPE) {
+	  running = false;
+	} else if(event.key.keysym.sym == SDLK_h) {
+	  visible = !visible;
+	  Cats::ShowSprite(spriteId, visible);
+	}
+      } else if(event.type == SDL_KEYUP) {
+      }
+    }
+
     float delta = (SDL_GetTicks() - lastFrameTime) / 1000.0f;
     lastFrameTime = SDL_GetTicks();
     Cats::Redraw(delta);
-    if(SDL_GetTicks() - startTime > 3000) {
-      running = false;
-    }
   }
 
   return 0;
